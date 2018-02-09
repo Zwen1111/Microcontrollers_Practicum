ed0b2fac842626536425cd03eab779fe23111a51
Zwen1111 <zwen12@msn.com>
2018-02-09 11:59:35 +0100

    B.3 excercise from Week 2 assignments

--- Week2/B.3/main.c
+++ Week2/B.3/main.c
@@ -3,15 +3,15 @@
 ** support, and with no warranty, express or implied, as to its usefulness for
 ** any purpose.
 **
-** main.c
+** lookup.c
 **
-** Beschrijving:	ISR on PORTD demonstrattion  
+** Beschrijving:	Ledpatroon op PORTD dmv table lookup (scheiding logica en data)    
 ** Target:			AVR mcu
-** Build:			avr-gcc -std=c99 -Wall -O3 -mmcu=atmega128 -D F_CPU=8000000UL -c ioisr.c
-**					avr-gcc -g -mmcu=atmega128 -o ioisr.elf ioisr.o
-**					avr-objcopy -O ihex ioisr.elf ioisr.hex 
+** Build:			avr-gcc -std=c99 -Wall -O3 -mmcu=atmega128 -D F_CPU=8000000UL -c switch.c
+**					avr-gcc -g -mmcu=atmega128 -o lookup.elf lookup.o
+**					avr-objcopy -O ihex lookup.elf lookup.hex 
 **					or type 'make'
-** Author: 			zwen12@msn.com
+** Author: 			mawijnen2@avans.nl & ajerkele@avans.nl
 ** -------------------------------------------------------------------------*/
 
 #define F_CPU 8000000
@@ -20,7 +20,36 @@
 #include <util/delay.h>
 #include <avr/interrupt.h>
 
-int j;
+int index;
+int vect1;
+int vect2;
+
+typedef struct { 
+	unsigned char data;
+} PATTERN_STRUCT; 
+
+// 7 seg
+// PORTD dp G F E D C B A
+//        y y y y x x x x
+
+PATTERN_STRUCT pattern[] = {
+	{0x3F}, // '0'
+	{0x06}, // '1'
+	{0x5B}, // '2'
+	{0x4F}, // '3'
+	{0x66}, // '4'
+	{0x6D}, // '5'
+	{0x7D}, // '6'
+	{0x07}, // '7'
+	{0x7F}, // '8'
+	{0x6F}, // '9'
+	{0x77}, // 'A'
+	{0x7C}, // 'B'
+	{0x39}, // 'C'
+	{0x5E}, // 'D'
+	{0x79}, // 'E'
+	{0x71}, // 'F'
+};
 
 /******************************************************************/
 void wait( int ms )
@@ -42,39 +71,30 @@ Version :    	DMK, Initial code
 
 /******************************************************************/
 ISR( INT1_vect )
-/* 
+/*
 short:			ISR INT1
-inputs:			
-outputs:	
-notes:			Turns of the LED and turns on the next LED
-Version :    	AJERKELE, Initial code
+inputs:
+outputs:
+notes:			Sets the vect1 variable to 1
+Version :    	Zwen van Erkelens & Thijs Wijnen, Initial code
 *******************************************************************/
 {
-	PORTC = 1<<j;
-	++j;
-	if(j>7){
-		j = 0;
-	}
-	wait(250);
+	vect1 = 1;
 }
 
 /******************************************************************/
 ISR( INT2_vect )
-/* 
+/*
 short:			ISR INT2
-inputs:			
-outputs:	
-notes:			Turns of the LED and turns on the next LED
-Version :    	AJERKELE, Initial code
+inputs:
+outputs:
+notes:			Sets the vect2 variable to 1
+Version :    	Zwen van Erkelens & Thijs Wijnen, Initial code
 *******************************************************************/
 {
-	PORTC = 1<<j;
-	++j;
-	if(j>7){
-		j = 0;
-	}
-	wait(250);
+	vect2 = 1;
 }
+
 
 /******************************************************************/
 int main( void )
@@ -82,27 +102,56 @@ int main( void )
 short:			main() loop, entry point of executable
 inputs:			
 outputs:	
-notes:			Slow background task after init ISR
-Version :    	DMK, Initial code
+notes:			
+Version :    	Zwen van Erkelens & Thijs Wijnen, Initial code
 *******************************************************************/
 {
-	// Init I/O
-	DDRD = 0xF0;			// PORTD(7:4) output, PORTD(3:0) input	
-	DDRC = 0xFF;			// PORTD(7:0) output
-
+	DDRD = 0xF0;
+	DDRC = 0b11111111;					// PORTD all output 
+	
+	// Set index to begin of pattern array
+	index = 0;
+	
 	// Init Interrupt hardware
 	EICRA |= 0x2C;			// INT2 falling edge, INT1 rising edge
 	EIMSK |= 0x06;			// Enable INT2 & INT1
 	
-	j = 0;
-	PORTC = 1<<j;
-	
 	// Enable global interrupt system
 	//SREG = 0x80;			// Of direct via SREG of via wrapper
 	sei();
-
-	while (1)
-	{								
+	
+	PORTC = pattern[index].data;
+	
+	while (1==1)
+	{
+		if (vect1 == 1 && vect2 == 1)
+		{
+			index = 0;
+			PORTC = pattern[index].data;
+			vect1 = 0;
+			vect2 = 0;
+		}else if (vect1 == 1)
+		{
+			index--;
+			if (index > 15 || index < 0)
+			{
+				PORTC = pattern[14].data;
+				}else{
+				PORTC = pattern[index].data;
+			}
+			vect1 = 0;
+		}else if(vect2 == 1)
+		{
+			index++;
+			if (index > 15 || index < 0)
+			{
+				PORTC = pattern[14].data;
+				}else{
+				PORTC = pattern[index].data;
+			}
+			vect2 = 0;
+		}
+		wait(200);
 	}
 
 	return 1;
---
+++ Week2/B.4/main.c
@@ -0,0 +1,18 @@
+/*
+ * B.4.c
+ *
+ * Created: 9-2-2018 11:53:38
+ * Author : zwen1
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
